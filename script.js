c3db2c982208da1f616f4d0b40263a0b82479752
Jose Ignacio Pareja <jose.ignacio97@gmail.com>
2022-03-21 18:53:15 -0300

    Finished with JS, Changed some properties of the CSS & HTML.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function playMusic(e) {
-    const audio = document.querySelector("audio[class='${e.code}']");
+    const audio = document.querySelector(`audio[class="${e.code}"]`);
     audio.currentTime = 0;
     audio.play();
-    const key = document.querySelector("div[class='key ${e.code}']");
+    const key = document.querySelector(`div[class="key ${e.code}"]`);
+    key.classList.add(`${e.code}playing`);
 }
 
 window.addEventListener('keydown', playMusic);
+
+function removeEffect(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove(`KeyMplaying`);
+    this.classList.remove(`KeyUplaying`);
+    this.classList.remove(`KeySplaying`);
+    this.classList.remove(`KeyIplaying`);
+    this.classList.remove(`KeyCplaying`);
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeEffect));
